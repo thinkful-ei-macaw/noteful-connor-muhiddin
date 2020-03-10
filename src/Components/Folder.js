0501488a26bdc2af9b82d87f24209d352a5619c0
Connor McCormack <mccormack.connor.m@gmail.com>
2020-03-10 10:17:55 -0500

    first commit

---
+++ src/Components/Folder.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Folder extends Component {
+  return (
+    <div className="Folder">
+      < />
+    </div>
+  );
+}
+
+export default Folder;
---
+++ src/Components/Note.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Note extends Component {
+  return (
+    <div className="Note">
+      < />
+    </div>
+  );
+}
+
+export default Note;
